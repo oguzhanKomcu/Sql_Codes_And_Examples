d957b9a0d0fe7f11550931f51f7e4fd8a0f72ecd
oguzhanKomcu <96787308+oguzhanKomcu@users.noreply.github.com>
2022-01-05 00:14:58 +0300

    Add files via upload

---
+++ UserDefinedFunction.sql
@@ -0,0 +1,60 @@
+use tire_and_rim_shop1
+
+
+--USER DEFÝNED FUNCTÝON
+
+---Let's make a function for our profit calculation.
+
+go
+create function KARHESAPLA(@satýþfiyatý MONEY , @alýþfiyatý money,  @stokmiktarý smallint) ---First we give the name of the function. Then we write it with the data types of the columns we will use in it.
+
+returns int  as  --We write the data type we want to convert.
+  begin return  (@satýþfiyatý - @alýþfiyatý )* @stokmiktarý  end --we apply the operation we want to do in our function.
+  go
+
+--WE CHECK OUR FUNCTION.
+  select   Diameter, 
+  sum(dbo.KARHESAPLA(SalePrice,ArrivalPrice,UnýtsInStock  ) )from tires group by Diameter
+
+
+ --- Let's make a function to capitalize letters in text
+  go
+create function buyukharf(@gelendeger nvarchar(50) )
+
+returns nvarchar(500)  as 
+  begin return  upper(@gelendeger)  end 
+  go
+
+---Is our function working? Let's check.
+  select SuppliersId, dbo.buyukharf(CompanyName) from Suppliers
+
+  select CompanyName, dbo.buyukharf(Address) from Suppliers
+
+
+
+
+--Let's prepare a function for the profit we get.
+go
+create function TOPLAMKAZANC(@birimfiyat int , @adet int  )
+   returns int 
+   as 
+   begin return ((@birimfiyat * @adet - (((@birimfiyat * @adet) /100) *18))) end 
+go
+   
+
+
+--Let's prepare a function for the profit we get.
+go
+create function TOPLAMKAZANC(@birimfiyat int , @adet int  )
+   returns int 
+   as 
+   begin return ((@birimfiyat * @adet - (((@birimfiyat * @adet) /100) *18))) end 
+go
+   
+
+---Is our function working? Let's check.
+
+select TiresId , dbo.TOPLAMKAZANC(TiresUnitPrice,TireQuantity) as Kazanc from OrdersDetails
+
+select RimsId , dbo.TOPLAMKAZANC(RimsUnitPrice,RimsQuantity) as Kazanc from OrdersDetails
+
