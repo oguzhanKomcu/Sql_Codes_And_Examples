9129c60049322f57840778d5bba7743ea2434ec8
Oğuzhan KÖMCÜ <96787308+oguzhanKomcu@users.noreply.github.com>
2023-02-24 00:59:18 +0300

    Update IndexQuery.sql

--- IndexQuery.sql
+++ IndexQuery.sql
@@ -8,3 +8,7 @@ CREATE NONCLUSTERED INDEX NameSurnameIndex1 ON CUSTOMERS
    NAMESURNAME
 )
 
+-- The code that shows how many lines are searched during the query in the message section
+SET statistics io on
+select * from CUSTOMERS WHERE NAMESURNAME LIKE 'Oğuzhan Kömcü'
+
