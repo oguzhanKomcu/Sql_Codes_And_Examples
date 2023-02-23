03b9b3537da9c571fc1a93ea3583b2060443e278
Oğuzhan KÖMCÜ <96787308+oguzhanKomcu@users.noreply.github.com>
2023-02-24 01:45:20 +0300

    Update IndexQuery.sql

--- IndexQuery.sql
+++ IndexQuery.sql
@@ -17,3 +17,12 @@ select * from CUSTOMERS WHERE NAMESURNAME LIKE 'Oğuzhan Kömcü'
 --Script showing table row and dimension information
 SP_SPACEUSED 'CUSTOMERS'
 
+
+--We also indexed a different column in the same table.
+--Note: There is a size cost in the index operation. If we want to make each column, we will get one more file size equal to the data size. This can also create costs.
+Use DATAABASE
+GO
+CREATE  NONCLUSTERED INDEX  tELnUMBERIndex1 on CUSTOMERS
+(
+   TELNR
+)
